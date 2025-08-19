c54961b3c108a8396ab8533efae261b0f51b277c
taiyifuyuan2 <taiyifuyuan2@gmail.com>
2025-08-19 17:31:20 +0900

    Fix ProtoSpace issues: remove unnecessary button, fix user links, correct form labels, center user names, add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :prototypes, dependent: :destroy
   has_many :comments, dependent: :destroy
+  
+  validates :name, presence: true
+  validates :profile, presence: true
+  validates :occupation, presence: true
+  validates :position, presence: true
 end
